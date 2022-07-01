06386841897d14b86b78f36023e926332b1ca98e
Duodu Randy <duodurandy19@gmail.com>
2022-07-01 05:48:55 +0000

    Added script to convert multiple files using pandoc

---
+++ pandoc_convert_files.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "========================================="
+echo "PANDOC CONVERT MULTIPLE FILES IN A FOLDER"
+echo "========================================="
+echo ""
+
+echo "Type in the following details"
+echo "-----------------------------"
+
+printf "Path to folder: "; read folder_path;
+printf "Input format: "; read input_fmt;
+printf "Output format: "; read output_fmt; 
+echo ""
+
+regex_pattern="^.+(\.$input_fmt)$";
+no_of_files_converted=0;
+
+if [[ ! -e "$folder_path/converted_files/" ]]; then
+  mkdir "$folder_path/converted_files";
+fi
+
+for file in $folder_path/*; do 		
+  filename=$(basename "$file");
+  #echo "$regex_pattern"
+	
+  if [[ $filename =~ $regex_pattern ]]; then
+  	let no_of_files_converted=$no_of_files_converted+1;
+  	pandoc -f $input_fmt -t $output_fmt $file -o "$folder_path/converted_files/${filename%.*}.$output_fmt";
+  	echo "* ${filename} - Done";
+  fi
+done
+
+echo ""
+echo "$no_of_files_converted '$input_fmt' files converted to '$output_fmt' files. Stored under '$folder_path/converted_files'."
+
+
+
